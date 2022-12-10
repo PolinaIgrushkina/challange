f4b764b616535a46a71769875fd525cea2b2ed9a
Polina Igrushkina <polina.igrushkina@gmail.com>
2022-12-10 22:45:16 +0300

    1

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,40 +1,47 @@
-import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
-import { itemsFetchData } from '../actions/items';
+import React, { Component, PropTypes } from "react";
+import { connect } from "react-redux";
+import { itemsFetchData } from "../actions/items";
+import SecondList from "./SecondList";
 
 class ItemList extends Component {
-    componentDidMount() {
-        this.props.fetchData('http://5af1eee530f9490014ead8c4.mockapi.io/items');
-    }
+  componentDidMount() {
+    this.props.fetchData("http://5af1eee530f9490014ead8c4.mockapi.io/items");
+  }
 
-    render() {
-        return (
-            <ul>
-                {this.props.items.map((item) => (
-                    <li key={item.id}>
-                        {item.label}
-                    </li>
-                ))}
-            </ul>
-        );
-    }
+  render() {
+    return (
+      <div
+        style={{
+          display: "flex",
+          gap: "20px",
+        }}
+      >
+        <ul>
+          {this.props.items.map((item) => (
+            <li key={item.id}>{item.label}</li>
+          ))}
+        </ul>
+        <SecondList />
+      </div>
+    );
+  }
 }
 
 ItemList.propTypes = {
-    fetchData: PropTypes.func.isRequired,
-    items: PropTypes.array.isRequired,
+  fetchData: PropTypes.func.isRequired,
+  items: PropTypes.array.isRequired,
 };
 
 const mapStateToProps = (state) => {
-    return {
-        items: state.items,
-    };
+  return {
+    items: state.items,
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        fetchData: (url) => dispatch(itemsFetchData(url))
-    };
+  return {
+    fetchData: (url) => dispatch(itemsFetchData(url)),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ItemList);
