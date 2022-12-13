124d58d67bf830912df1da2d70c5f757d0d7f3dc
Polina Igrushkina <polina.igrushkina@gmail.com>
2022-12-13 18:35:13 +0300

    Solved task

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,7 +1,17 @@
 import React, { Component, PropTypes } from "react";
 import { connect } from "react-redux";
 import { itemsFetchData } from "../actions/items";
-import SecondList from "./SecondList";
+
+const Item = ({ data }) => {
+  const { id, label, parent_id, children } = data;
+
+  return (
+    <ul>
+      <li style={{ paddingLeft: 10 }}>{label}</li>
+      {children && children.map((item) => <Item data={item} key={item.id} />)}
+    </ul>
+  );
+};
 
 class ItemList extends Component {
   componentDidMount() {
@@ -9,6 +19,16 @@ class ItemList extends Component {
   }
 
   render() {
+    const newArr = this.props.items.map((item) => {
+      return { ...item, children: [] };
+    });
+
+    const data = newArr.filter((item) => {
+      return !(
+        item.parent_id && newArr[item.parent_id - 1].children.push(item)
+      );
+    });
+
     return (
       <div
         style={{
@@ -21,7 +41,12 @@ class ItemList extends Component {
             <li key={item.id}>{item.label}</li>
           ))}
         </ul>
-        <SecondList />
+
+        <ul>
+          {data.map((item) => (
+            <Item data={item} key={item.id} />
+          ))}
+        </ul>
       </div>
     );
   }
